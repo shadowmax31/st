5aca564c885569da2ac27f98ac0ca0d7a79ed16e
Etienne Beaulieu <ety_95@hotmail.fr>
2019-03-23 03:00:06 -0400

    Changes for FreeBSD

--- config.mk
+++ config.mk
@@ -7,18 +7,20 @@ VERSION = 0.8.2
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/local/include
+X11LIB = /usr/local/lib
 
 PKG_CONFIG = pkg-config
 
+# freetype
+FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = /usr/local/include/freetype2
+
 # includes and libs
-INCS = -I$(X11INC) \
-       `$(PKG_CONFIG) --cflags fontconfig` \
-       `$(PKG_CONFIG) --cflags freetype2`
-LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft -lXrender\
+INCS = -I$(X11INC) -I$(FREETYPEINC)\
+       `$(PKG_CONFIG) --cflags fontconfig`
+LIBS = -L$(X11LIB) -lm -lrt -lX11 $(FREETYPELIBS) -lutil -lXft -lXrender\
        `$(PKG_CONFIG) --libs fontconfig` \
-       `$(PKG_CONFIG) --libs freetype2`
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
