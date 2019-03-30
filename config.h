c3ab0b32b8fc28db57c6b7bdf581a03f6ea1a579
Etienne Beaulieu <ety_95@hotmail.fr>
2019-03-30 17:39:03 -0400

    Change default term

--- config.h
+++ config.h
@@ -63,7 +63,8 @@ static unsigned int cursorthickness = 2;
 static int bellvolume = 0;
 
 /* default TERM value */
-char *termname = "st-256color";
+//char *termname = "st-256color";
+char *termname = "xterm";
 
 /*
  * spaces per tab
